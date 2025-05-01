427d39aac5156e0c5bda17e73ca7552d32e93f96
treasure-sky <ych0486666@gmail.com>
2025-05-02 01:08:07 +0900

    init: entry point 생성

---
+++ src/dev/jino/calculator/Lv1/App.java
@@ -0,0 +1,8 @@
+package dev.jino.calculator.Lv1;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Calculator!");
+    }
+}
---
+++ src/dev/jino/calculator/Lv3/App.java
@@ -0,0 +1,8 @@
+package dev.jino.calculator.Lv3;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Calculator!");
+    }
+}
