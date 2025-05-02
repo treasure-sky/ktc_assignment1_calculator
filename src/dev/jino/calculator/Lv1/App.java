5ebe7f09e6b7f809948d5a371e4c4e93a098c9c8
treasure-sky <ych0486666@gmail.com>
2025-05-02 14:31:16 +0900

    feat(Lv1): 입력기능 추가
    - 사용자로부터 숫자 두개를 입력받는 기능을 추가하였음.
    - 수는 0 - 2147483647 범위를 가지도록 제한하였음.

--- src/dev/jino/calculator/Lv1/App.java
+++ src/dev/jino/calculator/Lv1/App.java
@@ -1,8 +1,28 @@
 package dev.jino.calculator.Lv1;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        Scanner sc = new Scanner(System.in);
+
+        long num1, num2;
+        do {
+            System.out.print("첫 번째 숫자를 입력하세요(0 - 2147483647): ");
+            num1 = sc.nextLong();
+            if (num1 < 0 || num1 > 2147483647) {
+                System.out.printf("%d는 음수 입니다.\n", num1);
+            }
+        } while (num1 < 0);
+
+        do {
+            System.out.print("두 번째 숫자를 입력하세요(0 - 2147483647): ");
+            num2 = sc.nextLong();
+            if (num2 < 0 || num1 > 2147483647) {
+                System.out.printf("%d는 음수 입니다.\n", num1);
+            }
+        } while (num2 < 0);
+
     }
 }
